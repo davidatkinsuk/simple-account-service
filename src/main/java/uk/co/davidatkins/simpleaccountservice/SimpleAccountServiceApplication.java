01cda38c552497558a1e2e9e6a727369ac9a23cd
davidatkins <david.atkins@atos.net>
2018-06-04 19:53:46 +0100

    Initial commit

---
+++ src/main/java/uk/co/davidatkins/simpleaccountservice/SimpleAccountServiceApplication.java
@@ -0,0 +1,12 @@
+package uk.co.davidatkins.simpleaccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAccountServiceApplication.class, args);
+	}
+}
---
+++ src/main/java/uk/co/davidatkins/simpleaccountservice/model/Account.java
@@ -0,0 +1,41 @@
+package uk.co.davidatkins.simpleaccountservice.model;
+
+public class Account {
+
+    private String id;
+    private String firstName;
+    private String secondName;
+    private String accountNumber;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+}
