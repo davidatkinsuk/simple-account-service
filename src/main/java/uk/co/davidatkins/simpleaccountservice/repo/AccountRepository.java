5222fe1ffadfa8911260807d28303ec497612bf4
davidatkinsuk <david.atkins@atos.net>
2018-06-04 20:18:39 +0100

    Made test pass in simplest way possible

--- src/main/java/uk/co/davidatkins/simpleaccountservice/repo/AccountRepository.java
+++ src/main/java/uk/co/davidatkins/simpleaccountservice/repo/AccountRepository.java
@@ -1,4 +1,9 @@
 package uk.co.davidatkins.simpleaccountservice.repo;
 
+import uk.co.davidatkins.simpleaccountservice.model.Account;
+
+import java.util.List;
+
 public interface AccountRepository {
+    List<Account> getAccount();
 }
--- src/main/java/uk/co/davidatkins/simpleaccountservice/repo/InMemoryAccountRepository.java
+++ src/main/java/uk/co/davidatkins/simpleaccountservice/repo/InMemoryAccountRepository.java
@@ -1,4 +1,13 @@
 package uk.co.davidatkins.simpleaccountservice.repo;
 
+import uk.co.davidatkins.simpleaccountservice.model.Account;
+
+import java.util.Collections;
+import java.util.List;
+
 public class InMemoryAccountRepository implements AccountRepository {
+    @Override
+    public List<Account> getAccount() {
+        return Collections.emptyList();
+    }
 }
