92592b481ff811f73047caf675b210e5908c1855
davidatkinsuk <david.atkins@atos.net>
2018-06-04 20:46:11 +0100

    Added another test for retrieving accounts, this time with two.

--- src/test/java/uk/co/davidatkins/simpleaccountservice/repo/InMemoryAccountRepositoryTest.java
+++ src/test/java/uk/co/davidatkins/simpleaccountservice/repo/InMemoryAccountRepositoryTest.java
@@ -45,4 +45,43 @@ public class InMemoryAccountRepositoryTest {
 
     }
 
+    @Test
+    public void getAllAccounts_twoEntries() {
+
+        repo.addAccount(
+                Account.builder()
+                        .id(1)
+                        .accountNumber("account1")
+                        .firstName("firstName1")
+                        .secondName("secondName1")
+                        .build()
+        );
+
+        repo.addAccount(
+                Account.builder()
+                        .id(2)
+                        .accountNumber("account2")
+                        .firstName("firstName2")
+                        .secondName("secondName2")
+                        .build()
+        );
+
+        assertEquals(2,repo.getAccount().size());
+
+        Account firstAccount = repo.getAccount().get(0);
+        assertEquals(1,firstAccount.getId());
+        assertEquals("account1",firstAccount.getAccountNumber());
+        assertEquals("firstName1",firstAccount.getFirstName());
+        assertEquals("secondName1",firstAccount.getSecondName());
+
+        Account secondAccount = repo.getAccount().get(1);
+        assertEquals(2,secondAccount.getId());
+        assertEquals("account2",secondAccount.getAccountNumber());
+        assertEquals("firstName2",secondAccount.getFirstName());
+        assertEquals("secondName2",secondAccount.getSecondName());
+
+    }
+
+
+
 }
